4cdf55bf38e8acb9a375d1346ec0632b8aba7ddd
ishige-shogo <shigeshigeishige@gmail.com>
2020-11-07 04:29:04 +0000

    [update]validate

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,13 +10,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice]="Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      flash[:error]="error! Please check your Title and Body."
-      redirect_to books_path
+
+      @books = Book.all
+      render :index
     end
   end
 
@@ -29,10 +30,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice]="Book was successfully updated."
-    redirect_to "/books/#{book.id}"
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
+      redirect_to "/books/#{@book.id}"
+    else
+      render :edit
+    end
   end
 
   def destroy
